201c4053e8bdadc8dace0e5be63fb1ec9d2929b8
Angelo Cisneros <a.e.cisneros@gmail.com>
2015-02-04 20:50:58 -0800

    Wire up basic TodoCtrl in application.js, verify functionality on site root.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,17 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var TodoApp = angular.module("TodoApp", []);
+
+// config the HTTP module to include 
+// the X-CSRF_Token
+// X means its a custom header. Not a Standard header
+TodoApp.config(["$httpProvider", function($httpProvider){
+  $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content')
+}]);
+
+TodoApp.controller("TodoCtrl", ["$scope", "$http", function($scope, $http){
+  $scope.greeting = "Hello world";
+}])
