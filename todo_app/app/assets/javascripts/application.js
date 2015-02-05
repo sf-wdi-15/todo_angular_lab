64e2eaffacf1d0c3fdfcb4f1fbb263a07766366e
Alex Fulsome <alexfulsome@gmail.com>
2015-02-04 21:40:51 -0800

    something isnt working. gave in and compared to todays lesson. still can't find it. probably an obvious typo

---
+++ todo_app/app/assets/javascripts/application.js
@@ -0,0 +1,43 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+var TodoApp = angular.module("TodoApp", []);
+
+
+TodoApp.controller("TodoController", ["$scope", "$http", function ($scope, $http) {
+
+		  $http.get("/todos.json")
+		    .success(function (data) {
+		      console.log(data);
+		      $scope.todos = data;
+		    });
+
+  $scope.todos = [];
+
+$scope.addTodo = function () {
+	console.log($scope.newTodo);
+    $http.post("/todos.json", {todo: $scope.newTodo})
+      .success(function (data) {
+        console.log(data);
+        $scope.todos.push(data);
+        $scope.newTodo = {};
+
+
+      });
+  };
+
+}])
